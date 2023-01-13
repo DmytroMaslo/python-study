c5e70acbc42cfecb0684dbaee8d5e323c3fcca40
DmytroMaslo <maslo.dmitry@gmail.com>
2023-01-13 19:49:57 +0200

    if-else

--- start.py
+++ start.py
@@ -1,1 +1,11 @@
-print("Python installed and work correctly")
+message = "Python installed and work correctly123"
+environment = 'python'
+isChecked: bool = 1234
+
+print(message)
+print(isChecked)
+
+if (environment == 'python'):
+    print('Yes')
+else:
+    print('No')
