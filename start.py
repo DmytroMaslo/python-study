ec64b8e411c5f394beabc4bd86c329e11deab757
DmytroMaslo <maslo.dmitry@gmail.com>
2023-01-14 19:43:40 +0200

    array

--- start.py
+++ start.py
@@ -1,11 +1,17 @@
-message = "Python installed and work correctly123"
+message = "Python installed and work correctly"
 environment = 'python'
-isChecked: bool = 1234
 
 print(message)
-print(isChecked)
+
 
 if (environment == 'python'):
     print('Yes')
 else:
     print('No')
+
+browsers = ["chrome", "safari", "firefox","IE","opera"]
+browsers.append("Unknown")
+for browser in browsers:
+    print(browser)
+    
+print(browsers[0:7])
