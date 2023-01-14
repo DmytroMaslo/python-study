b42b40ca617b14abe26061be3e821c5a86286b24
DmytroMaslo <maslo.dmitry@gmail.com>
2023-01-14 19:48:51 +0200

    loop

--- start.py
+++ start.py
@@ -13,5 +13,18 @@ browsers = ["chrome", "safari", "firefox","IE","opera"]
 browsers.append("Unknown")
 for browser in browsers:
     print(browser)
-    
+
 print(browsers[0:7])
+configs = {
+    "browser":"opera",
+    "aut":"google site",
+    "test":"smoke",
+    "log":True
+}
+print(configs.get("test"))
+
+for conf in configs.values():
+    print(conf)
+
+if "browser" in configs:
+    print("Exist")
